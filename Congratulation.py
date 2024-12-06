dc4dd2f1508917ec6942aa2f0936a07bc7d3c82b
THSayeb <desktop499@gmail.com>
2024-12-06 23:12:58 +0600

    Congratulation

---
+++ Congratulation.py
@@ -0,0 +1,4 @@
+print("Hey there, \n It is a greeting simulator \n First  ")
+name = input("Enter your name ")
+message = "congratulation"
+print(" \n ___________________________________________________\n ", message.upper() , name.upper(), "!!!!")
